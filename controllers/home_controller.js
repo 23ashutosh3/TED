5e0cc9565f318461980fbb117c604e41566bbc14
23ashuT <AShukla@Moreyeahs.co>
2020-10-22 15:28:00 +0530

    first commit

---
+++ controllers/home_controller.js
@@ -0,0 +1,20 @@
+const Post = require('../models/post');
+
+module.exports.home = function(req, res) {
+
+    Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function(err, posts) {
+            return res.render('home', {
+                title: "Social | Home",
+                posts: posts
+            });
+        })
+
+}
